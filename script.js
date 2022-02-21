bc18f989e74971b65eec91b4c1ffe2cab964ae62
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-02-21 18:19:05 +0000

    Level 100 complete

--- script.js
+++ script.js
@@ -6,7 +6,38 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((show) => {
+    let season = show.season.toString();
+    let episode = show.number.toString();
+
+    const showContainer = document.createElement("div");
+    showContainer.classList.add("show");
+    rootElem.appendChild(showContainer);
+
+    const showTitleContainer = document.createElement("div");
+    showTitleContainer.classList.add("show-title-container");
+    showContainer.appendChild(showTitleContainer);
+
+    const showTitle = document.createElement("p");
+    showTitle.classList.add("show-title");
+    showTitle.innerText = `${show.name} - S${season.padStart(
+      2,
+      "0"
+    )}E${episode.padStart(2, "0")}`;
+    showTitleContainer.appendChild(showTitle);
+
+    const showThumbnail = document.createElement("img");
+    showThumbnail.classList.add("show-thumbnail");
+    showThumbnail.src = show.image.medium;
+    showContainer.appendChild(showThumbnail);
+
+    const showDescriptionContainer = document.createElement("div");
+    showDescriptionContainer.classList.add("show-description");
+    showDescriptionContainer.innerHTML = show.summary;
+    showContainer.appendChild(showDescriptionContainer);
+  });
 }
 
 window.onload = setup;
